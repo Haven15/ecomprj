4aa74bb633f8a6e20466e5f71952a078a0d3833c
rj-source-code <delacruz.robertjohnjayco@gmail.com>
2024-06-19 14:18:15 +0800

    Ep#5 Login Users using Django

--- userauths/views.py
+++ userauths/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import redirect, render
 from userauths.forms import UserRegisterForm
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
+from django.conf import settings
+
+#AUTH_USER_MODEL is from ecomprj/settings.py
+User = settings.AUTH_USER_MODEL
 
 def register_view(request):
     
@@ -24,3 +28,41 @@ def register_view(request):
         'form': form,
     }
     return render(request, "userauths/sign-up.html", context)
+
+def login_view(request):
+    if request.user.is_authenticated:
+        #Redirect to home page
+        messages.warning(request, "Hey you are already Logged In.")
+        return redirect("core:index")
+    
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        try:
+            #First email is from userauths/models.py
+            #Second email is local variable
+            user = User.objects.get(email=email)
+        except:
+            #f is formatted string
+            messages.warning(request, f"User with {email} does not exist.")
+
+        #Automatically login the user if login email exists.
+        #first email and password is from userauths/models.py
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            #Login function of django
+            login(request, user)
+            messages.success(request, "You are logged in.")
+            #Redirect to home page
+            return redirect("core:index")
+        else:
+            messages.warning(request, "User Does Not Exist. Create an account.")
+    
+    #Currently empty, we can add something in the future
+    context = {
+
+    }
+
+    return render(request, "userauths/sign-in.html", context)
